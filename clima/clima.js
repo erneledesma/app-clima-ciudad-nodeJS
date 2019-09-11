9e75ed50b63d061bf3faaffd1e8fc2dba6664678
Ernesto Ledesma <erneledesma@gmail.com>
2019-09-11 13:17:34 -0300

    Update clima.js

--- clima/clima.js
+++ clima/clima.js
@@ -3,7 +3,7 @@ const axios = require('axios')
 
 const getClima = async (lat, lng) => {
 
-    const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${ lat }&lon=${ lng }&appid=32f843d833c38373032f825c4a92418a&units=metric`)
+    const resp = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${ lat }&lon=${ lng }&appid=("Su Key")&units=metric`)
 
     return resp.data.main.temp
 
